39dbf39b5e205ff3d7e5cd8f21776ac4912f1223
Bharath kumar <123435792+Bharath-boop@users.noreply.github.com>
2026-03-15 23:52:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,9 +22,19 @@ const PORT = process.env.PORT || 5000;
 // ========== MIDDLEWARE ==========
 
 // Allow frontend to communicate with backend (CORS)
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://bhagyalakshmisilks.com"
+    ],
     credentials: true,
   })
 );
